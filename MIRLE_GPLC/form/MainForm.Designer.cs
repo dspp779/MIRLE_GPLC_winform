efe58b676207fb9e5e5309cef37e81f979e1c96a
dspp779 <dspp779@gmail.com>
2013-08-08 10:55:32 +0800

    New Build: initial commit Reference GMap.NET and SQLite.NET by NuGet Tool Prevent extent hell-like errors and warnings

---
+++ MIRLE_GPLC/form/MainForm.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace MIRLE_GPLC
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gMap = new GMap.NET.WindowsForms.GMapControl();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.latlngLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gMap
+            // 
+            this.gMap.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.gMap.Bearing = 0F;
+            this.gMap.CanDragMap = true;
+            this.gMap.EmptyTileColor = System.Drawing.Color.Navy;
+            this.gMap.GrayScaleMode = false;
+            this.gMap.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.gMap.LevelsKeepInMemmory = 5;
+            this.gMap.Location = new System.Drawing.Point(13, 13);
+            this.gMap.MarkersEnabled = true;
+            this.gMap.MaxZoom = 9;
+            this.gMap.MinZoom = 2;
+            this.gMap.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionWithoutCenter;
+            this.gMap.Name = "gMap";
+            this.gMap.NegativeMode = false;
+            this.gMap.PolygonsEnabled = true;
+            this.gMap.RetryLoadTile = 0;
+            this.gMap.RoutesEnabled = true;
+            this.gMap.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.gMap.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.gMap.ShowTileGridLines = false;
+            this.gMap.Size = new System.Drawing.Size(759, 627);
+            this.gMap.TabIndex = 0;
+            this.gMap.Zoom = 8D;
+            this.gMap.Load += new System.EventHandler(this.MainForm_Load);
+            this.gMap.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.gMap_MouseDoubleClick);
+            this.gMap.MouseMove += new System.Windows.Forms.MouseEventHandler(this.gMap_MouseMove);
+            this.gMap.MouseUp += new System.Windows.Forms.MouseEventHandler(this.gMap_MouseUp);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.latlngLabel});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 660);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(784, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // latlngLabel
+            // 
+            this.latlngLabel.Name = "latlngLabel";
+            this.latlngLabel.Overflow = System.Windows.Forms.ToolStripItemOverflow.Never;
+            this.latlngLabel.RightToLeftAutoMirrorImage = true;
+            this.latlngLabel.Size = new System.Drawing.Size(71, 17);
+            this.latlngLabel.Text = "latlngLabel";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 682);
+            this.Controls.Add(this.gMap);
+            this.Controls.Add(this.statusStrip1);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private GMap.NET.WindowsForms.GMapControl gMap;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel latlngLabel;
+    }
+}
+
---
+++ MIRLE_GPLC/form/MainForm.cs
@@ -0,0 +1,89 @@
+﻿using GMap.NET;
+using GMap.NET.WindowsForms;
+using GMap.NET.WindowsForms.Markers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MIRLE_GPLC
+{
+    public partial class MainForm : Form
+    {
+        private GMapOverlay markersOverlay;
+        private bool isDragging = false;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            this.gMap.DragButton = System.Windows.Forms.MouseButtons.Left;
+            this.gMap.ShowCenter = false;
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            // initialize map properties
+            gMap.MapProvider = GMap.NET.MapProviders.OpenStreetMapProvider.Instance;
+            GMap.NET.MapProviders.GMapProvider.Language = GMap.NET.LanguageType.ChineseTraditional;
+            GMap.NET.GMaps.Instance.Mode = GMap.NET.AccessMode.ServerOnly;
+            this.gMap.MapProvider.Copyright = "";
+
+
+            // intialize overlay
+            markersOverlay = new GMapOverlay("markers");
+            gMap.Overlays.Add(markersOverlay);
+        }
+
+        #region -- Mouse Events --
+
+        private void gMap_MouseMove(object sender, MouseEventArgs e)
+        {
+            PointLatLng latlng = gMap.FromLocalToLatLng(e.X, e.Y);
+            latlngLabel.Text = string.Format("({0:0.0}, {1:0.0})", latlng.Lat, latlng.Lng);
+            if (e.Button != MouseButtons.None)
+            {
+                isDragging = true;
+            }
+        }
+
+        private void gMap_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                gMap.Zoom++;
+            }
+        }
+
+        private void gMap_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (!isDragging && e.Button == MouseButtons.Right)
+            {
+                // click marker
+                foreach (GMapMarker marker in markersOverlay.Markers)
+                {
+                    if (marker.IsMouseOver)
+                    {
+                        markersOverlay.Markers.Remove(marker);
+                        return;
+                    }
+                }
+                markersOverlay.Markers.Clear();
+                GMarkerGoogle m = new GMarkerGoogle(gMap.FromLocalToLatLng(e.X, e.Y),
+                    GMarkerGoogleType.red_dot);
+                // allow map zooming
+                //marker.IsHitTestVisible = false;
+                // add to overlay
+                markersOverlay.Markers.Add(m);
+            }
+            isDragging = false;
+        }
+
+        #endregion
+
+    }
+}
