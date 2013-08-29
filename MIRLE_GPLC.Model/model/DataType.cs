c06ae86ca8d3cf97003ec03da26320c281418c9b
dspp779 <dspp779@gmail.com>
2013-08-29 15:31:25 +0800

    QQ

---
+++ MIRLE_GPLC.Model/model/DataType.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MIRLE_GPLC.Model
+{
+    public enum DataType
+    {
+        NONE = 0,
+        WORD = 1,
+        LONG = 2,
+        FLOAT = 3,
+        LONG_SWAP = 4,
+        FLOAT_SWAP = 5
+    }
+
+    public static class DataTypeUtil
+    {
+        public static ushort size(DataType type)
+        {
+            switch(type)
+            {
+                case DataType.NONE:
+                    return 0;
+                case DataType.WORD:
+                    return 1;
+                case DataType.LONG:
+                case DataType.FLOAT:
+                case DataType.LONG_SWAP:
+                case DataType.FLOAT_SWAP:
+                    return 2;
+                default:
+                    return 0;
+            }
+        }
+    }
+}
---
+++ MIRLE_GPLC.Model/model/Scaling.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MIRLE_GPLC.Model
+{
+    public class Scaling
+    {
+        private DataType _scale_type;
+        private double _raw_hi;
+        private double _raw_lo;
+        private double _scale_hi;
+        private double _scale_lo;
+
+        public Scaling(DataType scale_type, double raw_hi, double raw_lo, double scale_hi, double scale_lo)
+        {
+            this._raw_hi = raw_hi;
+            this._raw_lo = raw_lo;
+            this._scale_type = scale_type;
+            this._scale_hi = scale_hi;
+            this._scale_lo = scale_lo;
+        }
+
+        public Scaling(string scale_type, double raw_hi, double raw_lo, double scale_hi, double scale_lo)
+            : this((DataType)System.Enum.Parse(typeof(DataType), scale_type), raw_hi, raw_lo, scale_hi, scale_lo)
+        {
+        }
+    }
+}
